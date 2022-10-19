6a36c1b323613980825556c3fe788ee9e3bcfd5a
pmagalhaes2 <patricia.magalhaes2@gmail.com>
2022-10-19 19:42:39 -0300

    chore: add form validation

--- src/app/views/components/client/client-create/client-create.component.ts
+++ src/app/views/components/client/client-create/client-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormControl, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { Client } from "src/app/models/Client";
 import { ClientService } from "src/app/services/client.service";
@@ -16,21 +17,48 @@ export class ClientCreateComponent implements OnInit {
     phoneNumber: "",
   };
 
-  constructor(private router: Router, private service: ClientService) { }
+  name = new FormControl("", [Validators.minLength(5)]);
+  cpf = new FormControl("", [Validators.minLength(11)]);
+  phoneNumber = new FormControl("", [Validators.minLength(11)]);
 
-  ngOnInit(): void { }
+  constructor(private router: Router, private service: ClientService) {}
+
+  ngOnInit(): void {}
 
   create(): void {
-    this.service.create(this.client).subscribe((response) => {
-      this.router.navigate(["/clients"]);
-      this.service.returnMessage("Cliente criado com sucesso!");
-    },
+    this.service.create(this.client).subscribe(
+      (response) => {
+        this.router.navigate(["/clients"]);
+        this.service.returnMessage("Cliente criado com sucesso!");
+      },
       (err) => {
-        this.service.returnMessage(`ERRO: ${err.error.message}`)
-      });
+        this.service.returnMessage(`ERRO: ${err.error.message}`);
+      }
+    );
   }
 
   cancel(): void {
     this.router.navigate(["/clients"]);
+  }
+
+  errorInvalidName() {
+    if (this.name.invalid) {
+      return "O nome deve conter no mínimo 5 caracteres!";
+    }
+    return false;
+  }
+
+  errorInvalidCPF() {
+    if (this.cpf.invalid) {
+      return "O cpf deve conter no mínimo 11 caracteres!";
+    }
+    return false;
+  }
+
+  errorInvalidPhoneNumber() {
+    if (this.phoneNumber.invalid) {
+      return "O telefone deve conter no mínimo 11 caracteres!";
+    }
+    return false;
   }
 }
